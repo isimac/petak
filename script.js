dfa10f0b91a8a10e619c4dac93e78f5daf13b869
isimac <isimac@veleri.hr>
2023-03-14 16:47:22 +0100

    Slanje javascript datoteke

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
 $(document).ready(function(){
-    $("#character-form").submit(function(event){
-        event.preventDefault();
-        var imeLika=$("#character-name").val();
-        var url="https://swapi.dev/api/people/?search="+imeLika;
-        $.ajax({
-            url:url,
-            dataType:"json",
-            type: "GET",
-            success: function(data)
-            {
-                if (data.results.length>0)
-                {
-                    var lik=data.results[0];
-                    //Prikažite ime, godinu rođenja, spol, boju kose i očiju.
-                    var likInfo="<h1>"+lik.name+"</h1>";
-                    likInfo+="<p>"+lik.birth_year+"</p>";
-                    likInfo+="<p>"+lik.gender+"</p>";
-                    likInfo+="<p>"+lik.hair_color+"</p>";
-                    likInfo+="<p>"+lik.eye_color+"</p>";
-                    $("#character-info").html("<p>Podatci su pronađeni</p>");
-                }
-                else
-                {
-                    $("#character-info").html("<p>Podatci o liku nisu pronađeni</p>");
-                }
+  var pretragaInput=$("#pretraga-input");
+  var rezultatiPretrage=$("#rezultati-pretrage");
 
-            },
-            error:function(){
-                $("#character-info").html("<p>Podatci nisu dohvaćeni. Molimo pokušajte kasnije.</p>");
-            }
-        });
+  pretragaInput.on("input", function(event){
+    var pojamPretrage = event.target.value.trim();
+    var entity="song"; //potpuno nepotrebno
+    var pretragaurl="https://itunes.apple.com/search?term="+pojamPretrage+"&entity="+entity;
+    $.ajax({
+      url:pretragaurl,
+      method:"GET",
+      dataType:"json",
+      success: function(data){
+        rezultatiPretrage.empty();
+        if (data.resultCount==0)
+        {
+          rezultatiPretrage.text("Nije pronađeno podudaranje");
+        }
+        else
+        {
+          $.each(data.results, function(index, result){
+            var imePjesme=result.trackName;
+            var imeIzvodaca=result.artistName;
+
+            var pjesmaElement=$("<li></li>").text(imePjesme+" - "+imeIzvodaca);
+            rezultatiPretrage.append(pjesmaElement);
+          });
+        }
+      },
+      error: function(error){
+        console.log(error);
+        rezultatiPretrage.text("Problemi s dohvatom podataka. Molimo pokušajte kasnije.");
+      }
     });
+
+  });
 });
